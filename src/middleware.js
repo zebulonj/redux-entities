3fe62a1251654741fd9531825d6bd739d46c3293
Zebulon Young <zebulonj@gmail.com>
2017-04-28 08:46:18 -0700

    Parse incoming data.

--- src/middleware.js
+++ src/middleware.js
@@ -3,19 +3,30 @@ import {
   setEntity
 } from './actions';
 
-export const middleware = ({ save }) => store => {
-  const { dispatch } = store;
+const defaultOptions = {
+  parse: data => data
+};
 
-  return next => action => {
-    switch ( action.type ) {
-      case RESOURCES_SAVE_ENTITY:
-        return save( action.entity, dispatch )
-          .then( res => {
-            dispatch( setEntity( res ) );
-            return res;
-          });
-      default:
-        return next( action );
+export function middleware( options = {} ) {
+  options = Object.assign( {}, defaultOptions, options );
+
+  const { save, parse } = options;
+
+  return store => {
+    const { dispatch } = store;
+
+    return next => action => {
+      switch ( action.type ) {
+        case RESOURCES_SAVE_ENTITY:
+          return save( action.entity, dispatch )
+            .then( res => parse( res ) )
+            .then( res => {
+              dispatch( setEntity( res ) );
+              return res;
+            });
+        default:
+          return next( action );
+      }
     }
   }
 }
